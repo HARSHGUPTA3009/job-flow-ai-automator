f9bd0b8afe18532a39301f901680089d89d50c9b
HARSHGUPTA3009 <harsh30gupta09@gmail.com>
2025-11-18 16:29:23 +0530

    commit

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const User = require("../models/User");  // <-- DB model
+const User = require("");  // <-- DB model
 
 passport.use(
   new GoogleStrategy(
