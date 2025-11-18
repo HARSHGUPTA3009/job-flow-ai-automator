ebf807c88469bbadabd89716da0558f771327f66
HARSHGUPTA3009 <harsh30gupta09@gmail.com>
2025-11-18 15:58:41 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5001;
 // ============================================================================
 
 app.use(cors({
-  origin: 'https://jobflow-black.vercel.app',
+  origin: true,
   credentials: true
 }));
 
